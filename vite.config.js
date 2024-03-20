cb5ed3930ef2e6ca75ffb428945aa702303b967e
itzhake <itzikil@icloud.com>
2024-03-20 16:12:28 +0200

    1.3

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [vue(), VitePWA({
     registerType: 'autoUpdate',
-    // includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
     manifest: {
       name: 'My Awesome App',
       short_name: 'MyApp',
@@ -16,25 +16,25 @@ export default defineConfig({
       theme_color: '#ffffff',
       icons: [
         {
-          "src": "/@/pwa-192x192.png",
+          "src": "/journal-app-test/pwa-192x192.png",
           "sizes": "192x192",
           "type": "image/png",
           "purpose": "any"
         },
         {
-          "src": "/@/pwa-512x512.png",
+          "src": "/journal-app-test/pwa-512x512.png",
           "sizes": "512x512",
           "type": "image/png",
           "purpose": "any"
         },
         {
-          "src": "/@/pwa-maskable-192x192.png",
+          "src": "/journal-app-test/pwa-maskable-192x192.png",
           "sizes": "192x192",
           "type": "image/png",
           "purpose": "maskable"
         },
         {
-          "src": "/@/pwa-maskable-512x512.png",
+          "src": "/journal-app-test/pwa-maskable-512x512.png",
           "sizes": "512x512",
           "type": "image/png",
           "purpose": "maskable"
